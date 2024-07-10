0531babfdf2c0314e1eaf9aed2c71246e52aabc1
JonKadriu <jon.f.kadriu@gmail.com>
2024-07-10 11:50:11 +0200

    Add files via upload

---
+++ jonn/if-elif.py
@@ -0,0 +1,18 @@
+x = int(input("Shkruaje numrine pikeve:"))
+
+
+if ((x >= 0 ) and (x <= 29)):
+    print("Ti je ngelq edhe e ki noten 1")
+
+if ((x >= 30 ) and (x <= 49)):
+    print("Ti je dobet edhe e ki noten 2")
+
+if ((x >= 50 ) and (x <= 69)):
+    print("Ti je mir edhe e ki noten 3")   
+
+if ((x >= 70 ) and (x <= 89)):
+    print("Ti je shume i mire edhe e ki noten 4")  
+
+if ((x >= 90 ) and (x <= 100)):
+    print("Ti je i shkelqyeshem edhe e ki noten 5")
+    
---
+++ jonn/mesatarja.py
@@ -0,0 +1,13 @@
+
+x = input("A mundesh me jap nje numer:")
+
+
+if (int(x) > 0):
+    print(f"{x} eshte ma i madh se 0")
+
+elif (int(x) < 0):
+    print(f"{x} eshte ma i vogel se 0")
+
+else :
+    print(f"{x} eshte e barabarte me 0")
+
